18c2636bfc2090d8edb4c47e109e0dc9a82af3e7
chenshengfa <854852417@qq.com>
2020-03-23 23:44:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/okhttptest/NetCacheInterceptor.java
@@ -0,0 +1,31 @@
+package com.example.okhttptest;
+
+import android.text.TextUtils;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class NetCacheInterceptor  implements Interceptor {
+    public NetCacheInterceptor() {
+    }
+
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+
+        Request request = chain.request();
+        Response response = chain.proceed(request);
+
+        String cacheControl = request.cacheControl().toString();
+        if (TextUtils.isEmpty(cacheControl)) {
+            cacheControl = "public, max-age=60";
+        }
+        return response.newBuilder()
+                .header("Cache-Control", cacheControl)
+                .removeHeader("Pragma")
+                .build();
+    }
+}
